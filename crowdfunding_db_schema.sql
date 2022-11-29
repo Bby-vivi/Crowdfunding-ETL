ed37d76cf855ea73cf55122939393c89663e6bd5
Bby-vivi <114452770+Bby-vivi@users.noreply.github.com>
2022-11-29 15:30:53 -0500

    Add files via upload

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -12,13 +12,23 @@ CREATE TABLE "campaign" (
     "outcome" varchar(50)   NOT NULL,
     "backers_count" int   NOT NULL,
     "country" varchar(10)   NOT NULL,
-    "curency" varchar(10)   NOT NULL,
+    "currency" varchar(10)   NOT NULL,
     "launch_date" date   NOT NULL,
     "end_date" date   NOT NULL,
     "category_id" varchar(10)   NOT NULL,
-    "subcategory" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
      )
 );
 
@@ -38,13 +48,14 @@ CREATE TABLE "subcategory" (
      )
 );
 
-CREATE TABLE "contacts" (
-    "contact_id" int   NOT NULL,
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
     "email" varchar(100)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
      )
 );
 
@@ -54,6 +65,9 @@ REFERENCES "contacts" ("contact_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory" FOREIGN KEY("subcategory")
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
 
